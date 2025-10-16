07996918dff88ff217e68b891846f370fa772e14
Shiva Kumar Yadav <160407079+shivasky10@users.noreply.github.com>
2025-10-16 22:01:45 +0530

    patterns

--- Patterns.java
+++ Patterns.java
@@ -6,5 +6,37 @@ public class Patterns {
             }
             System.out.println();
         }
-    }
+        System.out.println("--------------------------------");
+        int n=5;
+        for(int i=1;i<=n;i++){
+            for(int s=1;s<=n-i+1;s++){
+                System.out.print("*");
+            }
+            System.out.println();
+
+        }
+        System.out.println("----------------------------------");
+        int a=4;
+        char ch='A';
+        for(int line=1;line<=a;line++){
+            for(int chars=1;chars<=line;chars++){
+                System.out.print(ch);
+                ch++;
+            }
+            System.out.println();
+        }
+        System.out.println("-----------------------------------");
+        int no=10;
+        int m=10;
+        for(int i=1;i<=no;i++){
+            for(int j=1;j<=m;j++){
+                if(i==1||i==no||j==1||j==m){
+                    System.out.print("*");
+                }else{
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+    } 
 }
