13c48d02aa619e089ba7f6a102ecfb44be884d8f
Shiva Kumar Yadav <160407079+shivasky10@users.noreply.github.com>
2025-10-16 21:03:14 +0530

    java

--- Loops.java
+++ Loops.java
@@ -1,5 +1,5 @@
 
-import java.util.Scanner;
+import java.util.*;
 
 public class Loops {
     public static void main(String[] args) {
@@ -26,18 +26,21 @@ public class Loops {
         System.out.println("enter number to check");
         int n = sc.nextInt();
         boolean isprime = true ;
-
-        for(int i=2;i<=n-1;i++){
-            if(n%i==0){
+        if(n==2){
+            System.out.println("is prime");
+        }else{
+        for(int i=2;i<=Math.sqrt(n);i++){ // sqrt of n avoids repeating of multiples
+            if(n%i==0){ //check is there any factors between 1 and number (n-1)
                 isprime=false;
             }
         }
-
+    }
         if(isprime==true){
             System.out.println(n+"is prime");
 
         }else{
             System.out.println(n +" is not a prime");
         }
+        
     }
 }
