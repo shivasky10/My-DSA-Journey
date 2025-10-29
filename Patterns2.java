fc021fdfeb793733ede646467acc20a698de0450
Shiva Kumar Yadav <160407079+shivasky10@users.noreply.github.com>
2025-10-29 19:07:06 +0530

    patterns

--- Patterns2.java
+++ Patterns2.java
@@ -52,11 +52,22 @@ public class Patterns2 {
             System.out.println();
         }
     }
-        
+      
+    public static void floyds(int n){
+        int count=1;
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=i;j++){
+                System.out.print(count+" ");
+                count++;
+            }
+            System.out.println();
+        }
+    }
+
     public static void main(String[] args) {
-   
-        hallow(4, 4);
-        invert_pyramid(4);
-        inverted_withnum(5);
+        // hallow(4, 4);
+        // invert_pyramid(4);
+        // inverted_withnum(5);
+        floyds(5);
     }
 }
