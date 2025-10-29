1d49d9c9c41545ea50954703c10b07f81ec24f67
Shiva Kumar Yadav <160407079+shivasky10@users.noreply.github.com>
2025-10-29 19:38:22 +0530

    patterns

--- Patterns2.java
+++ Patterns2.java
@@ -54,6 +54,11 @@ public class Patterns2 {
     }
       
     public static void floyds(int n){
+        // 1 
+        // 2 3
+        // 4 5 6
+        // 7 8 9 10
+        // 11 12 13 14 15
         int count=1;
         for(int i=1;i<=n;i++){
             for(int j=1;j<=i;j++){
@@ -64,10 +69,64 @@ public class Patterns2 {
         }
     }
 
+    public static void trianglebinary(int n){
+        // 1
+        // 01
+        // 101
+        // 0101
+        // 10101
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=i;j++){
+                if((i+j)%2==0){
+                    System.out.print("1");
+                }else{
+                    System.out.print("0");
+                }
+            }
+            System.out.println();
+        }
+    }
+
+
+    public static void butterfly(int n){
+        //first half
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=i;j++){
+                System.out.print("*");
+            }
+            //for spaces
+            for(int j=1;j<=2*(n-i);j++){
+                System.out.print(" ");
+            }
+            for(int j=1;j<=i;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        //second part inverted
+        for(int i=n;i>=1;i--){
+             for(int j=1;j<=i;j++){
+                System.out.print("*");
+            }
+            //for spaces
+            for(int j=1;j<=2*(n-i);j++){
+                System.out.print(" ");
+            }
+            for(int j=1;j<=i;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+
+
+
     public static void main(String[] args) {
         // hallow(4, 4);
         // invert_pyramid(4);
         // inverted_withnum(5);
-        floyds(5);
+        // floyds(5);
+        // trianglebinary(5);
+        butterfly(5);
     }
 }
