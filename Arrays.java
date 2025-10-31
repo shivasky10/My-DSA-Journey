c01a32ce3745a692048839a7a193a86e2ad53256
Shiva Kumar Yadav <160407079+shivasky10@users.noreply.github.com>
2025-10-31 21:01:19 +0530

    arrays

---
+++ Arrays.java
@@ -0,0 +1,32 @@
+import java.util.*;
+public class Arrays {
+    public static void update(int marks[]){
+        for(int i=0;i<marks.length;i++){
+            marks[i]+=1;
+        }
+
+    }
+    public static void main(String[] args) {
+        
+        // int marks[]=new int[100];
+        // //declaring size of an array
+        // Scanner sc = new Scanner(System.in);
+
+        // marks[0]=sc.nextInt();
+        // marks[1]=sc.nextInt();
+
+        // System.out.println("maths:"+marks[0]);
+        // System.out.println("physics:"+marks[1]);
+
+        int marks[]={98,97,95};
+        update(marks);
+
+        //here arrays is used as calllby reference so original value also changes but varibales are only call by value
+        for(int i=0;i<marks.length;i++){
+            System.out.print(marks[i]+" ");
+        }
+        System.out.println();
+       
+
+    }
+}
