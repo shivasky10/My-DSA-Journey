c43c8d850ce90aac43115d2535ebec6d3942fb4e
Shiva Kumar Yadav <160407079+shivasky10@users.noreply.github.com>
2025-11-03 17:16:58 +0530

    arrays

--- Binarysearch.java
+++ Binarysearch.java
@@ -18,10 +18,69 @@ public class Binarysearch {
 
         return -1;
     }
+
+
+
+    public static void  reverseArr(int number[]){
+        int first =0,last=number.length-1;
+        while(first<last){
+            int temp = number[first];
+            number[first]=number[last];
+            number[last]=temp;
+
+            first++;
+            last--;
+
+        }
+    }
+
+    public static void arrpairs(int number[]){
+        int t=0;
+        for(int i=0;i<number.length;i++){
+            int curr = number[i];
+            for(int j=i+1;j<number.length;j++){
+                System.out.print("("+curr +","+number[j]+")");
+                t++;
+            }
+            System.out.println();
+        }
+        System.out.println("total pairs:"+t);
+    }
+
+
+
+    //subarrays
+
+    public static void subarray(int number[]){
+
+        int t = (number.length*(number.length+1))/2;
+        for(int i=0;i<number.length;i++){
+            for(int j=i;j<number.length;j++){
+                for(int k=i;k<j;k++){
+                    System.out.print(number[k]+" ");
+                }
+                System.out.println();
+            }
+            System.out.println();
+        }
+        System.out.println("total subarrys:"+t);
+    }
     public static void main(String[] args) {
         int number[]={2,5,6,8,10,34,56};
         int key= 34;
 
-        System.out.println("At index" +binarysearch(number, key));
+        // System.out.println("At index" +binarysearch(number, key));
+
+
+        // reverseArr(number);
+        // for(int i=0;i<number.length;i++){
+        //     System.out.print(number[i]+ " ");
+        // }
+        // System.out.println();
+
+
+        // arrpairs(number);
+
+        subarray(number);
     }
 }
