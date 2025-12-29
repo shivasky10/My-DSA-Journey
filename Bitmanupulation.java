886e0feae4557e445e0ba9cf6467501a504a7d77
Shiva Kumar Yadav <160407079+shivasky10@users.noreply.github.com>
2025-12-29 06:56:20 +0530

    bits

--- Bitmanupulation.java
+++ Bitmanupulation.java
@@ -9,6 +9,29 @@ public class Bitmanupulation {
         }
     }
 
+    //operators in bits
+    //get ith bit
+    public static int getith(int n , int i){
+        int bitmask=1<<i;
+        if((n&bitmask)==0){
+            return 0;
+        }else{
+            return 1;
+        }
+    }
+
+    //count set bits ***imp
+    public static int countsetbits(int n){
+        int count=0;
+        while(n>0){
+            if((n&1)!=0){
+                count++;
+            }
+            n=n>>1;
+        }
+        return count;
+    }
+
 
     public static void main(String[] args) {
         // System.out.println(5&4);
@@ -17,7 +40,13 @@ public class Bitmanupulation {
         // System.out.println(~5);
         // System.out.println(5<<2);
         // System.out.println(6>>1);
-        oddoreven(5);
-        oddoreven(8);
+
+
+        // oddoreven(5);
+        // oddoreven(8);
+
+        // System.out.println(getith(10, 3));
+
+        System.out.println(countsetbits(10));
     }
 }
