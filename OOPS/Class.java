34944713f71048e50160f1ec38c51662a9a2c3fe
Shiva Kumar Yadav <160407079+shivasky10@users.noreply.github.com>
2025-12-31 15:58:56 +0530

    oops

--- OOPS/Class.java
+++ OOPS/Class.java
@@ -1,23 +1,37 @@
 package OOPS;
 
+
+// ----------------------------getters and setters---------
+
+
 public class Class {
     public static void main(String args[]){
         Pen p1=new Pen();
         p1.setcolor("yellow");
-        System.out.println(p1.color);
+        System.out.println(p1.getcolor());
+        p1.settip(10);
+        System.out.println(p1.gettip());
     }
 }
 
 
 class Pen{
-    String color;
-    int tip;
+    private String color;
+    private int tip;
 
-    void setcolor(String newcolor){
-        color=newcolor;
+    String getcolor(){
+        return this.color;
     }
 
-    void settip(int newtip){
-        tip=newtip;
+    int gettip(){
+        return this.tip;
+    }
+
+    void setcolor(String color){
+        this.color=color;
+    }
+
+    void settip(int tip){
+        this.tip=tip;
     }
 }
