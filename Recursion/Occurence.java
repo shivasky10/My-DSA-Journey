5139beaba0f635b24f290783a5d729873636ed4b
Shiva Kumar Yadav <160407079+shivasky10@users.noreply.github.com>
2026-01-25 14:47:53 +0530

    occurences

--- Recursion/Occurence.java
+++ Recursion/Occurence.java
@@ -14,8 +14,23 @@ public class Occurence {
 
         return firstoccurence(arr, key, i+1);
     }
+
+
+    public static int lastocuurence(int arr[],int key, int i){
+        if(i==arr.length){
+            return -1;
+        }
+
+        int isfound = lastocuurence(arr, key, i+1);  //recursion goes to end of array and gets -1;
+        if(isfound ==-1 && arr[i]==key){
+            return i;
+        } 
+
+        return isfound;
+    }
     public static void main(String[] args) {
         int arr[]={1,2,4,6,2,5,7,3,5};
-        System.out.println(firstoccurence(arr, 5,0 ));
+        // System.out.println(firstoccurence(arr, 5,0 ));
+        System.out.println(lastocuurence(arr, 5, 0));
     }
 }
